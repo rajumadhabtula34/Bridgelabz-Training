ef5f30cf665dc9649851baf7dd71d64aeb86284c
Nanivivo <nanivivo@gmail.com>
2026-06-19 17:32:50 +0530

    feat(Core-Basics): Palindrome logic is added

---
+++ day2/Palindrome.java
@@ -0,0 +1,22 @@
+package com.day2;
+
+import java.util.Scanner;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        int n=sc.nextInt();
+        int temp=n;
+        int rev=0;
+
+        for(;n>0;n/=10){
+            rev=rev*10+n%10;
+        }
+
+        if(temp==rev)
+            System.out.println("Palindrome");
+        else
+            System.out.println("Not Palindrome");
+    }
+}

f674fde8cb21fe86b4fde0f2a4354211e6bf9b55
Raju Madhabtula <nanivivo320@gmail.com>
2026-06-25 11:38:03 +0530

    Added GenerateRandomNumbersOfDice program

---
+++ diceroll/GenerateRandomNumbersOFDice.java
@@ -0,0 +1,13 @@
+package com.diceroll;
+
+public class GenerateRandomNumbersOFDice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+               int diceroll=(int)(Math.random()*6+1);
+               System.out.print(diceroll);
+               
+               
+	}
+
+}

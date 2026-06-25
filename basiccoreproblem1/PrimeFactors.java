f03632075d1dfa2da3d10b9545aafcabc6c223b4
Raju Madhabtula <nanivivo320@gmail.com>
2026-06-25 11:52:05 +0530

    Added PrimeFactors program

---
+++ basiccoreproblem1/PrimeFactors.java
@@ -0,0 +1,35 @@
+
+package com.basiccoreproblem1;
+
+import java.util.Scanner;
+
+public class PrimeFactors {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        if(n <= 1) {
+            System.out.println("Invalid Input");
+            return;
+        }
+
+        System.out.print("Prime Factors: ");
+
+        for(int i = 2; i * i <= n; i++) {
+
+            while(n % i == 0) {
+                System.out.print(i + " ");
+                n = n / i;
+            }
+        }
+
+        if(n > 1) {
+            System.out.print(n);
+        }
+
+        sc.close();
+    }
+}

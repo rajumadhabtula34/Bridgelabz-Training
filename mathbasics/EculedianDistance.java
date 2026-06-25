cbd1429d52c7a5d4802eef5491854aa19ce66244
Raju Madhabtula <nanivivo320@gmail.com>
2026-06-25 11:17:26 +0530

    Added EuclideanDistance program

---
+++ mathbasics/EculedianDistance.java
@@ -0,0 +1,14 @@
+package com.mathbasics;
+
+public class EculedianDistance {
+    public static void main(String[] args) {
+
+        int x=Integer.parseInt(args[0]);
+        int y=Integer.parseInt(args[1]);
+
+        double distance=
+                Math.sqrt(Math.pow(x,2)+Math.pow(y,2));
+
+        System.out.println("Distance = "+distance);
+    }
+}

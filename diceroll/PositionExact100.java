c52e33c4312e9ce777098945ff6dcebf8d60f43e
Raju Madhabtula <nanivivo320@gmail.com>
2026-06-25 11:38:36 +0530

    Added PositionExact100 program

---
+++ diceroll/PositionExact100.java
@@ -0,0 +1,42 @@
+package com.diceroll;
+
+public class PositionExact100 {
+	public static final int NO_PLAY=0;
+	public static final int LADDER=1;
+	public static final int SNAKE=2;
+
+	public static void main(String[] args) {
+		int position=0;
+		while(position < 100) {
+
+		    int dieRoll = (int)(Math.random() * 6) + 1;
+		    int option = (int)(Math.random() * 3);
+
+		    switch(option) {
+
+		        case LADDER:
+		            if(position + dieRoll <= 100) {
+		                position += dieRoll;
+		            }
+		            break;
+
+		        case SNAKE:
+		            position -= dieRoll;
+		            break;
+
+		        default:
+		            break;
+		    }
+
+		    if(position < 0) {
+		        position = 0;
+		    }
+		    if(position==100) {
+		    	System.out.print("won the match!!!" +position);
+		    	return ;
+		    }
+		}
+
+	}
+
+}

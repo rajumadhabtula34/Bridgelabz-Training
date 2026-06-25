f65795c48d734cc4646d150bc459dd135ba89a26
Raju Madhabtula <nanivivo320@gmail.com>
2026-06-25 11:05:15 +0530

    Added StringEquals program

---
+++ fundamentals/StringEquals.java
@@ -0,0 +1,19 @@
+package com.fundamentals;
+
+import java.util.Scanner;
+
+public class StringEquals {
+	public static void main(String[]args) {
+		  Scanner sc=new Scanner(System.in);
+		  String a=sc.nextLine();
+		  String b=sc.nextLine();
+		  if(a.equals(b)) {
+			    System.out.print("true");
+		  }
+		  else {
+				  
+		  System.out.println("false");
+	}
+	}
+
+}

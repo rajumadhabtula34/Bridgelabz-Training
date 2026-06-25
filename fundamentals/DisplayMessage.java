4bba2ba4dffbbb9cc747b657eb787a8b9c26b16d
Raju Madhabtula <nanivivo320@gmail.com>
2026-06-25 11:00:10 +0530

    added the display message program

---
+++ fundamentals/DisplayMessage.java
@@ -0,0 +1,10 @@
+package com.fundamentals;
+
+public class DisplayMessage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("hi good morning");
+	}
+
+}

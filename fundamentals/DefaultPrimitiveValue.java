2a6c60fb91734947309ce3470299af2f8a2fe241
Raju Madhabtula <nanivivo320@gmail.com>
2026-06-25 11:04:45 +0530

    Added DefaultPrimitiveValue program

---
+++ fundamentals/DefaultPrimitiveValue.java
@@ -0,0 +1,31 @@
+package com.fundamentals;
+class Demo{
+	    int a;
+	    byte b;
+	    short c;
+	    long d;
+	    float e;
+	    double f;
+	    char g;
+	    boolean h;
+	    
+	   
+}
+public class DefaultPrimitiveValue {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Demo d=new Demo();
+        System.out.println(d.a);
+        System.out.println(d.b);
+        System.out.println(d.c);
+        System.out.println(d.d);
+        System.out.println(d.e);
+        System.out.println(d.f);
+        System.out.println(d.g);
+        System.out.println(d.h);
+        
+		
+	}
+
+}

87faaf45a66241cea57b81dff647a91d2d080846
Raju Madhabtula <nanivivo320@gmail.com>
2026-06-25 11:19:38 +0530

    Added SumNaturalFor program

---
+++ mathbasics/SumNaturalFor.java
@@ -0,0 +1,18 @@
+package com.mathbasics;
+
+import java.util.Scanner;
+
+public class SumNaturalFor {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+
+        int sum=0;
+
+        for(int i=1;i<=n;i++){
+            sum+=i;
+        }
+
+        System.out.println(sum);
+    }
+}
